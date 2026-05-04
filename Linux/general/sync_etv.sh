eb2baa4675872c2d90cc2cb2034da03039964604
TCBW <1703161+TCBWZA@users.noreply.github.com>
2026-05-04 11:37:27 +0100

    Remove timestamp and permissions checks

--- Linux/general/sync_etv.sh
+++ Linux/general/sync_etv.sh
@@ -11,4 +11,4 @@ if ! mountpoint -q "/mnt/emedia"; then
     exit 1 
 fi
 
-rsync -avh --modify-window=5 --itemize-changes --progress --delete --exclude='*.tmp' "$SOURCE" "$DEST"
+rsync -avh --size-only --no-times --no-perms --omit-dir-times --modify-window=5 --itemize-changes --progress --delete --exclude='*.tmp' "$SOURCE" "$DEST"
