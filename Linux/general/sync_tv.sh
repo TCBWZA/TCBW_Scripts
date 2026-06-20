f8ef833b82222a62d3d6f9bccad67f0c49b0c09e
TCBW <1703161+TCBWZA@users.noreply.github.com>
2026-06-20 11:52:23 +0100

    Change away from CIFS mounted volume to a directly attached USB drive for the backups and sync's

--- Linux/general/sync_tv.sh
+++ Linux/general/sync_tv.sh
@@ -16,6 +16,15 @@ if ! grep -qs "/mnt/nmedia" /proc/mounts; then
     echo "ERROR: /mnt/nmedia did not mount. Aborting."
     exit 1
 fi
-# Deletes files in destination that no longer exist in source
-# parameters are to support copying to cifs mounted exFAT
-rsync -avh --size-only --no-times --no-perms --no-owner --no-group --no-times --omit-dir-times --itemize-changes --progress --delete "$SOURCE" "$DEST"
+
+rsync -avh --size-only --no-times --no-perms --no-owner --no-group --omit-dir-times --itemize-changes --progress --delete --inplace "$SOURCE" "$DEST"
+
+# Determine actual device backing /mnt/nmedia
+DEV=$(lsblk -no NAME,MOUNTPOINT \
+    | sed 's/^[^a-zA-Z0-9]*//' \
+    | awk '$2=="/mnt/nmedia"{print "/dev/"$1; exit}')
+
+echo "Flushing write buffers on $DEV..."
+sync
+blockdev --flushbufs "$DEV"
+
